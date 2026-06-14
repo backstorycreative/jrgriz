366d730d8414248f9ca7ba629f50c7d6e0911f8e
Backstory <hello@backstory.cc>
2026-06-14 17:59:26 +0000

    Replace JG square glyph with bear mark SVG in wordmark

--- app/components/Wordmark.tsx
+++ app/components/Wordmark.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Wordmark() {
   return (
     <Link href="/" className="mark" aria-label="JR Griz Football home">
-      <span className="glyph">JG</span>
+      <Image
+        src="/jr-griz-bear-mark.svg"
+        alt="JR Griz bear mark"
+        width={39}
+        height={44}
+        className="markbear"
+        priority
+        unoptimized
+      />
       <span className="wordtext">
         <span className="wt1">
           JR <b>GRIZ</b>
